e4e73835761c8f44c865fffe778e4acad6d6aabd
Ray <raymundolarasandoval@gmail.com>
2024-06-18 15:50:02 -0500

    Controllers y roues

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -28,15 +28,15 @@ export const getCliente = async (req,res)=> {
     res.json(rows[0]);
 }
 
-export const createCliente = async(req,res)=> {
+export const createFolioMuestreo = async(req,res)=> {
 
     try {
 
     
     const data = req.body
     //console.log(data)
-    const {rows} = await pool.query('INSERT INTO clientes(id,giro,nombre_empresa) values($1,$2,$3) RETURNING *',
-        [data.id,data.giro,data.nombre_empresa])
+    const {rows} = await pool.query('INSERT INTO folios_muestreos(folio, fecha, folio_cliente, folio_pdm) VALUES ($1, $2, $3, $4) RETURNING *', 
+        [data.folio,data.fecha,data.folio_cliente,data.folio_pdm])
     //console.log({rows})
     //res.send('Creando usuario')
     return res.json(rows[0])
@@ -73,14 +73,57 @@ export const updateCliente = async (req,res)=> {
 
 export const getPlanes = async(req,res)=> {
 
-    const {rows} = await pool.query('SELECT nombre_pdm from plandemuestreos order by id');
+    const {rows} = await pool.query('SELECT * FROM plandemuestreos WHERE DATE(fecha_hora_cita) = CURRENT_DATE ORDER BY id;');
     res.json(rows);
     //res.send('obteniendo usuarios');
     //console.log(rows);
 }
 
+export const getUltimoFolioMuestreo = async (req,res)=> {
+    //res.send('obteniendo el usuario con id:' + id)
+    const {rows} = await pool.query('select folio from folios_muestreos order by folio desc limit 1');
+    
+    if (rows.length === 0){
+    return res.status(404).json({message: "Error obteniendo ultimo folio "});
+    };
+    res.json(rows[0]);
+}
 
 export const getPlanInfo = async (req,res)=> {
+    const {id} = req.params;
+    //res.send('obteniendo el usuario con id:' + id)
+    const {rows} = await pool.query('select pdm.nombre_pdm,pdm.pq_atendera,pdm.folio_id_cot,pdm.fecha_hora_cita,pdm.ingeniero_campo from plandemuestreos pdm join ordenesdecompra ON ordenesdecompra.orden_id = pdm.ordencompra_id join clientes ON clientes.folio = ordenesdecompra.cliente_folio where pdm.nombre_pdm = $1',[id]);
+    
+    if (rows.length === 0){
+    return res.status(404).json({message: "Plan not found "});
+    };
+    res.json(rows[0]);
+}
+
+export const getPlanCliente = async (req,res)=> {
+    const {id} = req.params;
+    //res.send('obteniendo el usuario con id:' + id)
+    const {rows} = await pool.query('select clientes.giro,clientes.nombre_empresa,clientes.folio,clientes.direccion,clientes.estado,clientes.atencion,clientes.departamento,clientes.puesto,clientes.giro_empresa,clientes.telefono,clientes.correo,clientes.rfc from plandemuestreos pdm join ordenesdecompra ON ordenesdecompra.orden_id = pdm.ordencompra_id join clientes ON clientes.folio = ordenesdecompra.cliente_folio where pdm.nombre_pdm = $1',[id]);
+    
+    if (rows.length === 0){
+    return res.status(404).json({message: "Cliente not found "});
+    };
+    res.json(rows[0]);
+}
+
+
+export const getPlanServices = async (req,res)=> {
+    const {id} = req.params;
+    //res.send('obteniendo el usuario con id:' + id)
+    const {rows} = await pool.query('select id,cantidad,estudios_microbiologicos,estudios_fisicoquimicos,descripcion,cantidad_de_toma from servicios where pdm = $1 order by id',[id]);
+    
+    if (rows.length === 0){
+    return res.status(404).json({message: "Plan not found "});
+    };
+    res.json(rows);
+}
+
+export const getFolioInfo = async (req,res)=> {
     const {id} = req.params;
     //res.send('obteniendo el usuario con id:' + id)
     const {rows} = await pool.query('select * from plandemuestreos join servicios ON servicios.pdm = plandemuestreos.nombre_pdm where nombre_pdm = $1',[id]);
@@ -88,8 +131,65 @@ export const getPlanInfo = async (req,res)=> {
     if (rows.length === 0){
     return res.status(404).json({message: "Plan not found "});
     };
-    res.json(rows[0]);
+    res.json(rows);
 }
+
+
+
+export const getDescripciones = async(req,res)=> {
+
+    const {rows} = await pool.query('SELECT * from descripciones order by id');
+    res.json(rows);
+    //res.send('obteniendo usuarios');
+    //console.log(rows);
+}
+
+export const createMuestra = async (req, res) => {
+    try {
+      const data = req.body;
+  
+      // Query para insertar la muestra en la tabla 'muestras'
+      const query = `
+        INSERT INTO muestras(
+          registro_muestra, folio_muestreo, fecha_muestreo, hora_muestreo,
+          nombre_muestra, id_lab, cantidad_aprox, temperatura, lugar_toma,
+          descripcion_toma, e_micro, e_fisico, observaciones, folio_pdm,servicio_id)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,$15)
+        RETURNING *`;
+  
+      // Parámetros para el query
+      const values = [
+        data.registro_muestra,
+        data.folio_muestreo,
+        data.fecha_muestreo,
+        data.hora_muestreo,
+        data.nombre_muestra,
+        data.id_lab,
+        data.cantidad_aprox,
+        data.temperatura,
+        data.lugar_toma,
+        data.descripcion_toma,
+        data.e_micro,
+        data.e_fisico,
+        data.observaciones,
+        data.folio_pdm,
+        data.servicio_id
+      ];
+  
+      // Ejecutar el query usando pool.query
+      const { rows } = await pool.query(query, values);
+  
+      // Devolver la primera fila insertada como respuesta
+      return res.json(rows[0]);
+    } catch (error) {
+      // Manejar errores
+      console.error('Error en createMuestra:', error);
+      if (error.code === "23505") {
+        return res.status(409).json({ message: "Error de conflicto de llave única" });
+      }
+      return res.status(500).json({ message: "Error interno del servidor" });
+    }
+  };
 
 
 
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { pool } from '../db.js';
-import { getCliente,getClientes,createCliente, deleteCliente,updateCliente,getFolios,getPlanes, getPlanInfo} from '../controllers/clientes.controllers.js';
+import { getCliente,getClientes,createFolioMuestreo, deleteCliente,updateCliente,getFolios,getPlanes, getFolioInfo, getPlanServices, getDescripciones, getPlanInfo, getPlanCliente, getUltimoFolioMuestreo, createMuestra} from '../controllers/clientes.controllers.js';
 const router = Router();
 
-
+//largely-peaceful-polecat.ngrok-free.app
 //clientes
 router.get('/clientes',getClientes );
 
 router.get('/clientes/:id', getCliente);
 
-router.post('/clientes',createCliente );
+router.post('/crearfoliomuestreo',createFolioMuestreo );
+
 
 router.delete('/clientes/:id',deleteCliente);
 
@@ -18,7 +19,18 @@ router.put('/clientes/:id', updateCliente);
 //Planesdemuestreo
 router.get('/planes',getPlanes)
 
-router.get('/plan/:id',getPlanInfo)
+router.get('/planinfo/:id',getPlanInfo)
 
+router.get('/plancliente/:id',getPlanCliente)
+
+router.get('/plan/:id',getPlanServices)
+
+router.get('/folio/:id',getFolioInfo)
+
+router.get('/descripciones',getDescripciones)
+
+router.get('/ultimofoliomuestreo',getUltimoFolioMuestreo)
+
+router.post('/crearmuestra',createMuestra)
 
 export default router;
